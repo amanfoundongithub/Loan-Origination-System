1eefec0e1320c1ec1db327d91922828b8476d06f
Aman Raj <billygates124356@gmail.com>
2026-06-13 13:02:22 +0530

    added jwt

--- src/main/java/com/loan_org/gateway_service/jwt/impl/JwtValidatorImpl.java
+++ src/main/java/com/loan_org/gateway_service/jwt/impl/JwtValidatorImpl.java
@@ -1,16 +1,40 @@
 package com.loan_org.gateway_service.jwt.impl;
 
 import com.loan_org.gateway_service.jwt.JwtValidator;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
 
 @Service
 @Slf4j
 public class JwtValidatorImpl implements JwtValidator {
 
+    @Value("${jwt.signing_key}")
+    private String signingKey;
+
+    private SecretKey secretKey;
+
+    @PostConstruct
+    private void init() {
+        secretKey = Keys.hmacShaKeyFor(
+                signingKey.getBytes(StandardCharsets.UTF_8)
+        );
+    }
+
     @Override
     public void validate(String jwtToken) {
-        // will do this...
+        Jwts.parser()
+                .verifyWith(secretKey)
+                .build()
+                .parseSignedClaims(jwtToken)
+                .getPayload();
     }
 
 }
